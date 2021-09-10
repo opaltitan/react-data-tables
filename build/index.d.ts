400b23b13cf1944b55eec127d6c697f4d554d38f
Justin Patterson <patte2jd@gmail.com>
2021-09-10 15:36:19 -0400

    Refactored export structure

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,16 +1,14 @@
 /// <reference types="react" />
 import { DataFormatter } from 'simple-text-display-formatter';
 import { DATA_TABLE_FIELD_TYPE, DataTableParams, DataTableConfig } from './models/models';
-export declare namespace DataTable {
-    namespace SingleAxis {
-        const Component: (params: DataTableParams) => JSX.Element;
-        interface Params extends DataTableParams {
-        }
-        interface Config extends DataTableConfig {
-        }
+export declare namespace SingleAxis {
+    const Component: (params: DataTableParams) => JSX.Element;
+    interface Params extends DataTableParams {
     }
-    namespace Enums {
-        type FORMAT_TYPE = DataFormatter.FORMAT_TYPE;
-        type FIELD_TYPE = DATA_TABLE_FIELD_TYPE;
+    interface Config extends DataTableConfig {
     }
+}
+export declare namespace DataTableEnums {
+    type FORMAT_TYPE = DataFormatter.FORMAT_TYPE;
+    type FIELD_TYPE = DATA_TABLE_FIELD_TYPE;
 }
--- build/index.js
+++ build/index.js
@@ -1,8 +1,5 @@
 import { DataTableSingleAxis } from './SingleAxis';
-export var DataTable;
-(function (DataTable) {
-    var SingleAxis;
-    (function (SingleAxis) {
-        SingleAxis.Component = DataTableSingleAxis;
-    })(SingleAxis = DataTable.SingleAxis || (DataTable.SingleAxis = {}));
-})(DataTable || (DataTable = {}));
+export var SingleAxis;
+(function (SingleAxis) {
+    SingleAxis.Component = DataTableSingleAxis;
+})(SingleAxis || (SingleAxis = {}));
