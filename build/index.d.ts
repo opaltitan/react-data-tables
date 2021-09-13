da68ca8df5de3c6cbcef70c9e6ce39d6597727d7
Justin Patterson <patte2jd@gmail.com>
2021-09-13 18:32:44 -0400

    Added Fields model to SingleAxis namespace export

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,12 +1,13 @@
 /// <reference types="react" />
 import { DataFormatter } from 'simple-text-display-formatter';
-import { DATA_TABLE_FIELD_TYPE, DataTableParams, DataTableConfig } from './models/models';
+import { DATA_TABLE_FIELD_TYPE, DataTableParams, DataTableConfig, DataTableField } from './models/models';
 export declare namespace SingleAxis {
     const Component: (params: DataTableParams) => JSX.Element;
     interface Params extends DataTableParams {
     }
     interface Config extends DataTableConfig {
     }
+    type Field = DataTableField;
 }
 export declare namespace DataTableEnums {
     type FORMAT_TYPE = DataFormatter.FORMAT_TYPE;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { DataFormatter } from 'simple-text-display-formatter';
-import { DATA_TABLE_FIELD_TYPE, DataTableParams, DataTableConfig } from './models/models';
+import { DATA_TABLE_FIELD_TYPE, DataTableParams, DataTableConfig, DataTableField } from './models/models';
 import { DataTableSingleAxis } from './SingleAxis';
 
-export namespace DataTable {
-  export namespace SingleAxis {
-    export const Component = DataTableSingleAxis;
+export namespace SingleAxis {
+  export const Component = DataTableSingleAxis;
 
-    export interface Params extends DataTableParams {}
-    export interface Config extends DataTableConfig {}
-  }
+  export interface Params extends DataTableParams {}
+  export interface Config extends DataTableConfig {}
+  export type Field = DataTableField;
+}
 
-  export namespace Enums {
-    export type FORMAT_TYPE = DataFormatter.FORMAT_TYPE;
-    export type FIELD_TYPE = DATA_TABLE_FIELD_TYPE;
-  }
+
+export namespace DataTableEnums {
+  export type FORMAT_TYPE = DataFormatter.FORMAT_TYPE;
+  export type FIELD_TYPE = DATA_TABLE_FIELD_TYPE;
 }
